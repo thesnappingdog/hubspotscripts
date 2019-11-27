c6d6cbf2e272bf60c5790eecabd4ce1ac9f7c07b
thesnappingdog <richard.a.makara@gmail.com>
2019-11-27 17:05:06 +0200

    typo company props get and create

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config(object):
     HAPIKEY = os.environ.get('HAPIKEY_MAIN')
     HAPIKEY_SAND = os.environ.get('HAPIKEY_SAND')
-    HAUTH = {'hapikey': HAPIKEY_SAND}
+    HAUTH_SAND = {'hapikey': HAPIKEY_SAND}
+    HAUTH_PROD = {'hapikey': HAPIKEY}
 
     FLOW_BASE_URL = "https://api.hubapi.com/automation/v3/workflows/"
     COMPANY_PROPS_URL = "https://api.hubapi.com/properties/v1/companies/properties/"
--- property_updates/company_properties.py
+++ property_updates/company_properties.py
@@ -3,8 +3,7 @@ from config import Config
 
 
 def request_all_properties():
-    params = {'hapikey': Config.HAPIKEY_SAND}
-    return requests.get(Config.COMPANY_PROPS_URL, params=params).json()
+    return requests.get(Config.COMPANY_PROPS_URL, params=Config.HAPIKEY_SAND).json()
 
 
 def parse_properties(data, group):
@@ -20,8 +19,7 @@ def parse_properties(data, group):
 
 
 def create_property(property):
-    params = {'hapikey': Config.HAPIKEY}
-    return requests.post(Config.COMPANY_PROPS_URL, json=property, params=params)
+    return requests.post(Config.COMPANY_PROPS_URL, json=property, params=Config.HAUTH_PROD)
 
 
 def create_all_properties(properties):
@@ -34,6 +32,5 @@ if __name__ == "__main__":
     data = request_all_properties()
     group = 'data_infrastructure'
     properties = parse_properties(data, group)
-    #print(properties[0])
     create_all_properties(properties)
 
